186ab5d6ffa8e6cec9ae145faa62efa2a9bd10e1
MBOUAMBOUA Yvon <yvon.mbouamboua@gmail.com>
2026-06-02 18:11:53 +0200

    update module.R

--- R/modules.R
+++ R/modules.R
@@ -41,8 +41,8 @@
       "KISS1R","TAC3","TACR3",
       "GNRHR","PCSK1","PCSK2",
       "SCG2","CHGA","CHGB",
-      "CPE","VGF","SYP","RAB3A",
-      "ESR1","PGR","AR" # hormone
+      "CPE","VGF","SYP","RAB3A"
+      #"ESR1","PGR","AR" # hormone
     ), genes)
   )
 }
